58aeda00405d890f4c00f7fde03ac8987d4621a3
Nowaaru <zackyboy135@gmail.com>
2026-02-15 19:46:29 -0700

    feat: typescript-ify

---
+++ index.d.ts
@@ -0,0 +1,57 @@
+import Signal from "@rbxts/signal";
+
+interface Voxel extends Part {}
+interface Hitbox 
+{
+    Touched: Signal<(baseParts: BasePart[]) => void>,
+
+    Start(): void,
+
+    Stop(): void,
+
+    Destroy(): void,
+
+    GetTouchingParts(): void,
+
+    WeldTo(): void,
+
+    UnWeld(): void,
+}
+
+interface MoveableHitbox extends Hitbox
+{
+    Part: Part,
+}
+
+export function VoxelizePart(partToVoxelize: Part, minimumVoxelSize: number, timeToReset: number): Voxel[]
+
+export function CreateMoveableHitbox(
+    minimumVoxelSize?: number, 
+    voxelResetTime?: number,
+    hitboxSize?: Vector3, 
+    hitboxCoordinate?: CFrame, 
+    hitboxShape?: Enum.PartType, 
+    queryParameters?: OverlapParams
+): MoveableHitbox
+
+export function CreateHitbox(
+    hitboxSize?: Vector3,
+    hitboxCoordinate?: CFrame,
+    hitboxShape?: Enum.PartType,
+    minimumVoxelSize?: number,
+    voxelResetTime?: number,
+    queryParameters?: OverlapParams,
+): Voxel[]
+
+export function CutInHalf(partToCut: Part): [Part, Part]
+
+export const VoxbreakerSettings: {
+    // The name of attribute that the module will check parts for.
+    TagName: string 
+    RandomColors: boolean  // Will make every part a random color. Set this to true if you want a visual representation of how the parts are being divided.
+    Visualizer: boolean // Will make hitboxes visible when set to true
+    miniumCubeSize: number //Default Minimum possible size that divided cubes can be.
+    voxelFolder: Workspace //Where the voxels are stored. Workspace by default.
+    AutoStartMoveable: boolean //Toggle this to true if you want your moveable hitboxes to activate automatically without having to use :Start()
+    PartCacheEnabled: boolean //Enables PartCache. This significantly improves performance so I reccommend keeping it on. 
+}
